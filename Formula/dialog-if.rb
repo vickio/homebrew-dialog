087eb65c749b05368484b0c633393b5cb7556232
Chris Vickio <chris@vickio.net>
2019-09-01 21:12:22 +0300

    dialog-if 0h/01 (new formula)

---
+++ Formula/dialog-if.rb
@@ -0,0 +1,23 @@
+class DialogIf < Formula
+  desc "Domain-specific language for creating works of interactive fiction"
+  homepage "https://linusakesson.net/dialog/"
+  url "https://hd0.linusakesson.net/files/dialog-0h01_0_30.zip"
+  version "0h01"
+  sha256 "7a5fe94fdef3d5d7749517b971dfd70d2ae8d7bcf1d3be949060e557a201a1ae"
+
+  def install
+    chdir "src" do
+      system "make"
+      bin.install "dgdebug", "dialogc"
+    end
+    doc.install Dir["docs/*"]
+    prefix.install "readme.txt", "license.txt"
+    pkgshare.install "stdlib.dg", "stddebug.dg"
+  end
+
+  test do
+    (testpath/"test.dg").write("(program entry point) hello world")
+    system bin/"dialogc", "-tz8", "test.dg"
+    assert_predicate testpath/"test.z8", :exist?
+  end
+end
