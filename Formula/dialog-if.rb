3d6e49d27933b9a6725cde418e17212888c4c8cc
Chris Vickio <chris@vickio.net>
2019-09-24 20:26:20 +0300

    dialog-if 0h03

--- Formula/dialog-if.rb
+++ Formula/dialog-if.rb
@@ -1,9 +1,9 @@
 class DialogIf < Formula
   desc "Domain-specific language for creating works of interactive fiction"
   homepage "https://linusakesson.net/dialog/"
-  url "https://hd0.linusakesson.net/files/dialog-0h02_0_30.zip"
-  version "0h02"
-  sha256 "7236e5735cc287ad50f3611d536420592284f5eb1a5febec5127cbc68a0cbf93"
+  url "https://hd0.linusakesson.net/files/dialog-0h03_0_30.zip"
+  version "0h03"
+  sha256 "ab6ee52ef2e4fff768035d18ae9ac54be8153df455fd6647f69c30a9d771a188"
 
   def install
     chdir "src" do
