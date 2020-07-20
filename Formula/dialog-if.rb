0d54664500132b8b8ea258704bd792733082a087
Chris Vickio <chris@vickio.net>
2020-07-20 07:47:23 +0300

    Update dialog-if from 0k04 to 0k05

--- Formula/dialog-if.rb
+++ Formula/dialog-if.rb
@@ -1,9 +1,9 @@
 class DialogIf < Formula
   desc "Domain-specific language for creating works of interactive fiction"
   homepage "https://linusakesson.net/dialog/"
-  url "https://hd0.linusakesson.net/files/dialog-0k04_0_41.zip"
-  version "0k04"
-  sha256 "48eb067c835a2a734ba8aa18a58941c001910ba7b0d8a8cdbf031bfff4fe622e"
+  url "https://hd0.linusakesson.net/files/dialog-0k05_0_42.zip"
+  version "0k05"
+  sha256 "8e0844c47ccf7d768c4d2d15c2f5df1517d3e5c89ab04172ce7af3090087210e"
 
   def install
     chdir "src" do
