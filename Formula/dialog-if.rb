3d1df86b006293b7c9b306daff4f8122909eeb15
Chris Vickio <chris@vickio.net>
2026-03-31 21:55:40 +0200

    Update dialog-if.rb
    Disable formula with message about community fork

--- Formula/dialog-if.rb
+++ Formula/dialog-if.rb
@@ -1,23 +1,15 @@
 class DialogIf < Formula
   desc "Domain-specific language for creating works of interactive fiction"
   homepage "https://linusakesson.net/dialog/"
-  url "https://hd0.linusakesson.net/files/dialog-0m03_0_46.zip"
-  version "0m03"
-  sha256 "fd0781553e25f6372a45fad81967fc1f30f8215b7cc79bc25434bb8192697e2f"
+
+  disable! date: "2025-03-31", because: "a community-maintained fork is now available at dialog-if/brew"
 
   def install
-    chdir "src" do
-      system "make"
-      bin.install "dgdebug", "dialogc"
-    end
-    doc.install Dir["docs/*"]
-    prefix.install "readme.txt", "license.txt"
-    pkgshare.install "stdlib.dg", "stddebug.dg"
-  end
-
-  test do
-    (testpath/"test.dg").write("(program entry point) hello world")
-    system bin/"dialogc", "-tz8", "test.dg"
-    assert_predicate testpath/"test.z8", :exist?
+    odie <<~EOS
+      This tap is deprecated. Please migrate to the community-maintained tap:
+        brew untap vickio/dialog
+        brew tap dialog-if/brew
+        brew install dialog-if/brew/dialog-if
+    EOS
   end
 end
